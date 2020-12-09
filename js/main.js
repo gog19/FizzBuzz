7b3f8598b53e04b407151f353b31e727ba3c16ec
gayratyuldoshev-go <devinfinitum0@gmail.com>
2020-12-09 23:23:55 +0500

    add remover

--- js/main.js
+++ js/main.js
@@ -18,29 +18,34 @@ formElement.addEventListener('submit', function (evt) {
         result.style.padding = '10px';
         result.style.borderRadius = '20px';
         result.style.backgroundColor = 'yellow';
+        input.value = '';
     } else if ((input.value % 3 === 0) && (input.value % 5 === 0)) {
         result.textContent = 'FizzBuzz';
         result.style.color = 'blue';
         result.style.padding = '10px';
         result.style.borderRadius = '20px';
         result.style.backgroundColor = 'pink';
+        input.value = '';
     } else if (input.value % 3 === 0) {
         result.textContent = 'Fizz';
         result.style.color = 'orange';
         result.style.padding = '10px';
         result.style.borderRadius = '20px';
         result.style.backgroundColor = 'green';
+        input.value = '';
     } else if (input.value % 5 === 0) {
         result.textContent = 'Buzz';
         result.style.color = 'brown';
         result.style.padding = '10px';
         result.style.borderRadius = '20px';
         result.style.backgroundColor = 'lightblue';
+        input.value = '';
     } else if ((input.value % 3 !== 0) && (input.value % 5 !== 0)) {
         result.textContent = input.value;
         result.style.color = 'white';
         result.style.padding = '10px';
         result.style.borderRadius = '20px';
         result.style.backgroundColor = 'brown';
+        input.value = '';
     }
 })
