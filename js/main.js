5da5d743ae3b6d54366d4e2fb584fec862051ece
gayratyuldoshev-go <devinfinitum0@gmail.com>
2020-12-09 22:44:57 +0500

    Add functional part

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ var result = document.querySelector('.demo');
 formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    if (input.value % 3 === 0) {
+    if ((input.value % 3 === 0) && (input.value % 5 === 0)) {
+        result.textContent = 'FizzBuzz';
+    } else if (input.value % 3 === 0) {
         result.textContent = 'Fizz';
     } else if (input.value % 5 === 0) {
         result.textContent = 'Buzz';
-    } else if (input.value % 3 === 0 && input.value % 5 === 0) {
-        result.textContent = 'FizzBuzz';
+    } else if ((input.value % 3 !== 0) && (input.value % 5 !== 0)) {
+        result.textContent = input.value
     }
 })
