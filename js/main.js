034591f6683028729948121e95493ae6eeb0f598
gayratyuldoshev-go <devinfinitum0@gmail.com>
2020-12-09 22:40:18 +0500

    set new file for normailze

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var formElement = document.querySelector('.form-elements');
+var input = formElement.querySelector('.form-input');
+var result = document.querySelector('.demo');
+
+formElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    if (input.value % 3 === 0) {
+        result.textContent = 'Fizz';
+    } else if (input.value % 5 === 0) {
+        result.textContent = 'Buzz';
+    } else if (input.value % 3 === 0 && input.value % 5 === 0) {
+        result.textContent = 'FizzBuzz';
+    }
+})
