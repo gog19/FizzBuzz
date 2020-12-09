658066fe027d15f3a85e26348870c7ba0f6275bf
gayratyuldoshev-go <devinfinitum0@gmail.com>
2020-12-09 23:20:48 +0500

    Create styles

--- js/main.js
+++ js/main.js
@@ -5,13 +5,42 @@ var result = document.querySelector('.demo');
 formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    if ((input.value % 3 === 0) && (input.value % 5 === 0)) {
+
+    if (isNaN(input.value)) {
+        result.textContent = 'YOU CAN WRITE ONLY NUMBERS';
+        result.style.color = 'pink';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'blue';
+    } else if (input.value === '') {
+        result.textContent = 'WRITE SOMETHING';
+        result.style.color = 'red';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'yellow';
+    } else if ((input.value % 3 === 0) && (input.value % 5 === 0)) {
         result.textContent = 'FizzBuzz';
+        result.style.color = 'blue';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'pink';
     } else if (input.value % 3 === 0) {
         result.textContent = 'Fizz';
+        result.style.color = 'orange';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'green';
     } else if (input.value % 5 === 0) {
         result.textContent = 'Buzz';
+        result.style.color = 'brown';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'lightblue';
     } else if ((input.value % 3 !== 0) && (input.value % 5 !== 0)) {
-        result.textContent = input.value
+        result.textContent = input.value;
+        result.style.color = 'white';
+        result.style.padding = '10px';
+        result.style.borderRadius = '20px';
+        result.style.backgroundColor = 'brown';
     }
 })
